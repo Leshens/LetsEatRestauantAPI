99f51a42eb4406bbbd90fb044ad1040a24164a24
kacpe <kacper.kamm@gmail.com>
2023-11-13 18:48:13 +0100

    first commit

---
+++ src/main/java/com/leshen/LetsEatRestauantAPI/LetsEatRestauantApiApplication.java
@@ -0,0 +1,13 @@
+package com.leshen.LetsEatRestauantAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsEatRestauantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsEatRestauantApiApplication.class, args);
+	}
+
+}
