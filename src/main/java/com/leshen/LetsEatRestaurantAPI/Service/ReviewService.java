d6540060d8ee83aae2cd798cbf43d916c282c55b
leshens <kacper.kamm@gmail.com>
2024-01-19 13:50:41 +0100

    fix, zły token w testach

--- src/main/java/com/leshen/LetsEatRestaurantAPI/Service/ReviewService.java
+++ src/main/java/com/leshen/LetsEatRestaurantAPI/Service/ReviewService.java
@@ -7,7 +7,6 @@ import com.leshen.LetsEatRestaurantAPI.Repository.RestaurantRepository;
 import com.leshen.LetsEatRestaurantAPI.Repository.ReviewRepository;
 import com.leshen.LetsEatRestaurantAPI.Service.Mappers.RestaurantMapper;
 import com.leshen.LetsEatRestaurantAPI.Service.Mappers.ReviewMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
