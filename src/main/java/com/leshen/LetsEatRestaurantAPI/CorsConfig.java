615760d40599ce673bcd1591fd3e476fd2f8c3fb
leshens <kacper.kamm@gmail.com>
2024-01-19 15:37:55 +0100

    CORS

---
+++ src/main/java/com/leshen/LetsEatRestaurantAPI/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.leshen.LetsEatRestaurantAPI;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+        // Add CORS configuration for WebSocket handshake
+        registry.addMapping("/ws/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
