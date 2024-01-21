6c82467c2493097062dfb5a9350b395707c5c294
sugarvalley <cqrkowa@gmail.com>
2024-01-21 20:35:46 +0100

    swagger for tables

--- src/main/java/com/leshen/LetsEatRestaurantAPI/Contract/TableDto.java
+++ src/main/java/com/leshen/LetsEatRestaurantAPI/Contract/TableDto.java
@@ -1,5 +1,6 @@
 package com.leshen.LetsEatRestaurantAPI.Contract;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TableDto {
+    @Schema(description = "Unique identifier for the table")
     private Long tableId;
+
+    @Schema(description = "Identifier for the restaurant to which the table belongs")
     private Long restaurantId;
+
+    @Schema(description = "Token for authorization purposes", defaultValue = "your_token", example = "your_token")
     private String token;
+
+    @Schema(description = "Size of the table", minimum = "2", example = "2", defaultValue = "2")
     private Short size;
 }
--- src/main/java/com/leshen/LetsEatRestaurantAPI/Controller/TablesController.java
+++ src/main/java/com/leshen/LetsEatRestaurantAPI/Controller/TablesController.java
@@ -1,9 +1,15 @@
 package com.leshen.LetsEatRestaurantAPI.Controller;
 
+import com.leshen.LetsEatRestaurantAPI.Contract.MenuDto;
 import com.leshen.LetsEatRestaurantAPI.Contract.RestaurantDto;
 import com.leshen.LetsEatRestaurantAPI.Contract.ReviewDto;
 import com.leshen.LetsEatRestaurantAPI.Service.TableService;
 import com.leshen.LetsEatRestaurantAPI.Contract.TableDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,34 +30,60 @@ public class TablesController {
     private TableService tableService;
 
     @PostMapping
+    @Operation(summary = "Add a new table")
+    @ApiResponse(responseCode = "200", description = "Table created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TableDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
     public ResponseEntity<Long> newTable(@RequestBody TableDto newTableDto) {
         Long id = tableService.createTable(newTableDto);
         return ResponseEntity.created(URI.create("/table/" + id)).build();
     }
 
     @GetMapping
+    @Operation(summary = "Get all tables")
+    @ApiResponse(responseCode = "200", description = "Tables found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TableDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
     public ResponseEntity<List<TableDto>> getAllTables() {
         List<TableDto> tableDtos = tableService.getAllTables();
         return new ResponseEntity<>(tableDtos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TableDto> getById(@PathVariable long id) {
+    @Operation(summary = "Get a table by ID")
+    @ApiResponse(responseCode = "200", description = "Table found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TableDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Table not found", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    public ResponseEntity<TableDto> getById(@Parameter(description="Identifier of the table") @PathVariable long id) {
         Optional<TableDto> tableDto = tableService.getTableById(id);
         return tableDto.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Table not found"));
     }
 
     @GetMapping("/restaurant/{restaurantId}")
-    public ResponseEntity<List<TableDto>> getTablesForRestaurant(@PathVariable Long restaurantId) {
+    @Operation(summary = "Get tables for a specific restaurant")
+    @ApiResponse(responseCode = "200", description = "Tables found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TableDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Tables not found", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    public ResponseEntity<List<TableDto>> getTablesForRestaurant(@Parameter(description="Identifier of the restaurant") @PathVariable Long restaurantId) {
         List<TableDto> tables = tableService.getTablesForRestaurant(restaurantId);
         return new ResponseEntity<>(tables, HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Update a table")
+    @ApiResponse(responseCode = "200", description = "Table updated", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TableDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "401", description = "Unauthorized, bad token", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Table not found", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
     public ResponseEntity<TableDto> patchTable(
+            @Parameter(description="Identifier of the table")
             @PathVariable Long id,
             @RequestBody TableDto tableDto,
+            @Parameter(description="Authorization token")
             @RequestHeader("Authorization") String requestToken) {
         try {
             if (!tableService.verifyToken(id, requestToken)) {
@@ -71,8 +103,16 @@ public class TablesController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Delete a table")
+    @ApiResponse(responseCode = "200", description = "Table deleted", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Long.class)))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "401", description = "Unauthorized, bad token", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Table not found", content = @Content)
+    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
     public ResponseEntity<Long> deleteTable(
+            @Parameter(description="Identifier of the table")
             @PathVariable Long id,
+            @Parameter(description="Authorization token")
             @RequestHeader("Authorization") String requestToken) {
         try {
             if (!tableService.verifyToken(id, requestToken)) {
